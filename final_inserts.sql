1e1b9bf67dc3ec32fdce7e6fa8747cf73ad83832
Kalen Shaw <54906281+Kalen1Shaw@users.noreply.github.com>
2022-04-19 16:50:44 -0400

    Create final_inserts.sql

---
+++ final_inserts.sql
@@ -0,0 +1,25 @@
+USE finaldb;
+
+-- Inserts for Teams Table
+INSERT INTO Teams(TEAM_ID, TEAM_NAME, SCHOOL, CITY, STATE) VALUES
+(1, `Steelers`, `Morgan State`, `Pittsburgh`, `PA`),
+();
+
+--Inserts for Players Table
+INSERT INTO
+
+-- Inserts for Matches Table
+INSERT INTO 
+
+-- Inserts for Match_Stats Table
+INSERT INTO
+
+-- Inserts for Player_Stats Table
+INSERT INTO 
+
+
+-- Inserts for PLayer_Team Table
+INSERT INTO 
+
+--Inserts for Mode Table
+INSERT INTO
