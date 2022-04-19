9e0fbf9ab49705c68b460f1267ca3d6fb0d1b964
Kalen Shaw <54906281+Kalen1Shaw@users.noreply.github.com>
2022-04-19 18:13:45 -0400

    Update final_inserts.sql

--- final_inserts.sql
+++ final_inserts.sql
@@ -11,7 +11,7 @@ INSERT INTO Teams(TEAM_ID, TEAM_NAME, SCHOOL, CITY, `STATE`) VALUES
 INSERT INTO Players(PLAYER_ID, FIRST_NAME, LAST_NAME, `NUMBER`, USERNAME, CONSOLE, GENDER, DOB) VALUES
 (100, `Kalen`, `Shaw`, 44, `KingFlashTV`, `PS`, `M`, `1998-07-23`),
 (101, `Timothy`, `Richardson`, 12, `TKRich`, `PC`, `M`, `1997-03-21`),
-(102, `Ashley`, `Williams`, 41, `AWillDoIt`, `Xbox`, `F`, `2001-04-10`),
+(102, `Michael`, `Woodward`, 41, `MikeAtFamu`, `Xbox`, `F`, `2001-04-10`),
 (103, `Jamie`, `Sharpe`, 00, `SharpGaming`, `PC`, `NS`, `2002-05-26`),
 (104, `Kiari`, `Cephus`, 32, `Offsetyrn`, `PS`, `M`, `1996-12-14`),
 (105, `Latabia`, `Woodward`, 50, `landkshaw`, `PC`, `F`, `1999-06-06`),
@@ -19,22 +19,19 @@ INSERT INTO Players(PLAYER_ID, FIRST_NAME, LAST_NAME, `NUMBER`, USERNAME, CONSOL
 (107, `Kalen`, `Brown`, 18, `Brownsville`, `PS`, `M`, `1999-08-23`),
 (108, `Belcalis`, `Cephus`, 53, `iamcardib`, `PC`, `F`, `1996-06-14`),
 (109, `Jaylan`, `Holman`, 65, `JHolman`, `Xbox`, `M`, `1998-09-16`),
-(110, `Nikki`, `Isley`, `IsleWin`, `PS`, `NS`, `2003-08-27`),
-(111, `Carlos`, `Theran`, 42, `TDawg`, `PC`, `M`, `1997-09-11`),
-(112, `Jordan`, `Cephus`, 18, `JCeph`, `PS`, `M`, `2000-01-25`)
-(113),
-(114),
-(115);
+(110, `Reggie`, `Marrow`, `Made2Playa`, `PS`, `NS`, `2003-08-27`),
+(111, `Diamond`, `Bishop`, 42, `LadyJefe`, `PC`, `M`, `1997-09-11`),
+(112, "J'Briana", `Davis`, 18, `SummerBoss`, `PS`, `M`, `2000-01-25`)
+(113, `Kevin`, `Shaw`, 26, `Kannon1`, `Xbox`, `M`, `2001-04-19`),
+(114, `Jamaurya`, `Morris`, 12, `jamaurya`, `PS`, `M`, `1996-06-17`),
+(115, `Sandra`, `Goodwin`, 25, `SweetPea`, `Xbox`, `F`, `1997-01-26`);
 
 -- Inserts for Matches Table
 INSERT INTO Matches(MATCH_ID, HOME_TEAM, AWAY_TEAM, SCORE, `DATE`,`TIME`, RD1MODE, RD2MODE, RD3MODE) VALUES
-(),
-(),
-(),
-(),
-(),
-(),
-();
+(1111, 1, 3, `2-0`, `2022-01-21`, `14:30`, 40, 42, NULL),
+(2222, 2, 4, `1-2`, `2022-01-22`, `12:00`, 43, 44, 40),
+(3333, 1, 4, `2-1`, `2022-01-23`, `15:50`, 41, 43, 42),
+(4444, 2, 3, `2-0`, `2022-01-23`, `13:15`, 44, 42, NULL);
 
 -- Inserts for Match_Stats Table
 INSERT INTO Match_Stats(MATCH_ID, HOMERD1, AWAYRD1, HOMERD2, AWAYRD2, HOMERD3, AWAYRD3) VALUES
@@ -88,4 +85,5 @@ INSERT INTO Mode(MODE_CODE, MODE_NAME, DESCRIPT) VALUES
 (42, `Hardpoint`, `Secure a periodically-rotating objective while defending it from the opponent.`),
 (43, `Search and Destroy`, `Teams take turns defending and destroying an objective. No respawn.`),
 (44, `Control`, `Two teams alternate between attacking or defending two fixed capture zones on the map with limited respawns. 
+Attackers either have to capture both zones or completely eliminate the Defenders before the time limit.`);mited respawns. 
 Attackers either have to capture both zones or completely eliminate the Defenders before the time limit.`);
